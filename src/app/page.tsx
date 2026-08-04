b0418b08f4873ef64bf0818ddeaf09fe165e253e
pastorswcf-a11y <pastor.swcf@gmail.com>
2026-08-04 12:49:23 -0400

    Added this week's ministry opportunities to the homepage Happening section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,6 +64,14 @@ const happeningCards: {
   },
   {
     enabled: true,
+    startsAt: "2026-08-04T00:00:00-04:00",
+    expiresAt: "2026-08-05T19:00:00-04:00",
+    title: "This Week's Ministry Opportunities",
+    image: "/this-weeks-ministry-opportunities.jpg",
+    alt: "This week's ministry opportunities at Still Water Christian Fellowship",
+  },
+  {
+    enabled: true,
     startsAt: "2026-07-22T00:00:00-04:00",
     expiresAt: "2026-08-14T13:00:00-04:00",
     title: "Vacation Bible School",
